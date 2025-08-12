e936a6a61e23cc4c6e36a85a40074a1d5a4cfb1e
Pablo <cv.juanp@gmail.com>
2025-08-11 18:25:32 -0700

    UPDATE user db schema

--- server/server/Data/Models/User.cs
+++ server/server/Data/Models/User.cs
@@ -1,18 +1,23 @@
-
+using System;
 using System.ComponentModel.DataAnnotations;
+
 public class User
 {
     [Key]
     public int id { get; set; }
+
     [Required]
     [MinLength(4)]
     [MaxLength(20)]
-    public string name { get; set; }
+    public string name { get; set; } = string.Empty;
+
     [Required]
     [MaxLength(255)]
-    public string hashedPass { get; set; }
+    public string hashedPass { get; set; } = string.Empty;
+
     [Required]
     public int role { get; set; }
+
     [Required]
     public DateTime createdAt { get; set; }
 }
