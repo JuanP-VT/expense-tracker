d640f82a87606402fb68598df8666a15a40b62f0
Pablo <cv.juanp@gmail.com>
2025-08-14 12:34:31 -0700

    ADD NoFutureDateTime data annotation

---
+++ server/server/Data/Annotations/NoFutureDateTime.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Data.Annotations
+{
+    public class NoFutureDateTime : ValidationAttribute
+    {
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult($"{validationContext.DisplayName} cannot be null");
+            }
+
+            if ((DateTime)value > DateTime.UtcNow)
+            {
+                return new ValidationResult($"{validationContext.DisplayName} cannot be a future date");
+            }
+
+            // This is the correct way to return a successful validation
+            return ValidationResult.Success;
+        }
+    }
+}
