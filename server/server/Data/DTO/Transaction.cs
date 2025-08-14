a32cfddd80abe777d3c264000b56d8f83dff477a
Pablo <cv.juanp@gmail.com>
2025-08-14 12:38:26 -0700

    ADD transaction DTO

---
+++ server/server/Data/DTO/Transaction.cs
@@ -0,0 +1,23 @@
+﻿using server.Data.Annotations;
+using server.Data.Enums;
+using System.ComponentModel.DataAnnotations;
+
+namespace server.Data.DTO
+{
+    public class Transaction
+    {
+        [Required]
+        public TransactionType Type { get; set; }
+        [HigherThanCero]
+        public decimal Amount { get; set; }
+
+        [Required]
+        public string Category { get; set; } = string.Empty;
+
+        [NoFutureDateTime]
+        public DateTime Date { get; set; }
+
+        [Required]
+        public string Description { get; set; } = string.Empty;
+    }
+}
