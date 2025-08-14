822e2427cd4cc92942fb844d9b85d0ef04c0f1dd
Pablo <cv.juanp@gmail.com>
2025-08-13 17:52:31 -0700

    ADD data annotation to validate integers that are higher than cero

---
+++ server/server/Data/Annotations/HigherThanCero.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server.Data.Annotations
+{
+    public class HigherThanCero : ValidationAttribute
+    {
+
+        protected override ValidationResult? IsValid(object? value, ValidationContext validationContext)
+        {
+            if (value == null)
+            {
+                return new ValidationResult($"{validationContext.DisplayName} cannot be null");
+            }
+
+            if ((decimal)value <= 0)
+            {
+                return new ValidationResult($"{validationContext.DisplayName} must be greater than zero");
+            }
+
+            // This is the correct way to return a successful validation
+            return ValidationResult.Success;
+        }
+    }
+}
